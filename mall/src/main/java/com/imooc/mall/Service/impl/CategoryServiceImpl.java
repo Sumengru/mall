c32a315ac6667ffda671f1565a750dc933f318f8
宿梦茹 <1719292390@qq.com>
2022-04-09 13:09:54 +0800

    递归实现查询ID和parent_id

--- mall/src/main/java/com/imooc/mall/Service/impl/CategoryServiceImpl.java
+++ mall/src/main/java/com/imooc/mall/Service/impl/CategoryServiceImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
-
 import static com.imooc.mall.consts.MallConst.ROOT_PARENT_ID;
 
 @Service
@@ -21,6 +21,7 @@ import static com.imooc.mall.consts.MallConst.ROOT_PARENT_ID;
 public class CategoryServiceImpl implements ICategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
+
     @Override
     public ResponseVo<List<CategoryVo>> selectAll() {
         List<Category> categories = categoryMapper.selectAll();
@@ -37,28 +38,44 @@ public class CategoryServiceImpl implements ICategoryService {
                 .map(this::category2CategoryVO)
                 .sorted(Comparator.comparing(CategoryVo::getSortOrder).reversed())
                 .collect(Collectors.toList());
-        findSubCategory(categoryVos,categories);
+        findSubCategory(categoryVos, categories);
         return ResponseVo.success(categoryVos);
     }
 
-    public void findSubCategory(List<CategoryVo> categoryVoList,List<Category> categoryList){
-        for(CategoryVo categoryVo : categoryVoList){
+    @Override
+    public void findSubCategoryId(Integer id, Set<Integer> resultSet) {
+        List<Category> categoryList = categoryMapper.selectAll();
+        findSubCategoryId(id, resultSet, categoryList);
+    }
+
+    public void findSubCategoryId(Integer id, Set<Integer> resultSet, List<Category> categoryList) {
+        for (Category category : categoryList) {
+            if (category.getParentId().equals(id)) {
+                resultSet.add(category.getId());
+                findSubCategoryId(category.getId(), resultSet, categoryList);
+            }
+        }
+    }
+
+    public void findSubCategory(List<CategoryVo> categoryVoList, List<Category> categoryList) {
+        for (CategoryVo categoryVo : categoryVoList) {
             List<CategoryVo> subCategoryList = new ArrayList<>();
-            for(Category category : categoryList){
-                if(categoryVo.getId().equals(category.getParentId())){
+            for (Category category : categoryList) {
+                if (categoryVo.getId().equals(category.getParentId())) {
                     CategoryVo subCategory = category2CategoryVO(category);
                     subCategoryList.add(subCategory);
                 }
             }
             subCategoryList.sort(Comparator.comparing(CategoryVo::getSortOrder).reversed());
             categoryVo.setSubCategories(subCategoryList);
-            findSubCategory(subCategoryList,categoryList);
+            findSubCategory(subCategoryList, categoryList);
         }
 
     }
-    public CategoryVo category2CategoryVO(Category category){
+
+    public CategoryVo category2CategoryVO(Category category) {
         CategoryVo categoryVo = new CategoryVo();
-        BeanUtils.copyProperties(category,categoryVo);
+        BeanUtils.copyProperties(category, categoryVo);
         return categoryVo;
     }
 }
--- mall/src/test/java/com/imooc/mall/Service/impl/CategoryServiceImplTest.java
+++ mall/src/test/java/com/imooc/mall/Service/impl/CategoryServiceImplTest.java
@@ -5,13 +5,17 @@ import com.imooc.mall.Service.ICategoryService;
 import com.imooc.mall.enums.ResponseEnums;
 import com.imooc.mall.vo.CategoryVo;
 import com.imooc.mall.vo.ResponseVo;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
+@Slf4j
 public class CategoryServiceImplTest extends MallApplicationTests {
     @Autowired
     private ICategoryService categoryService;
@@ -20,5 +24,12 @@ public class CategoryServiceImplTest extends MallApplicationTests {
     public void selectAll() {
         ResponseVo<List<CategoryVo>> listResponseVo = categoryService.selectAll();
         assert (ResponseEnums.SUCCESS.getCode() == listResponseVo.getStatus());
+    }
+
+    @Test
+    public void findSubCategoryId() {
+        Set<Integer> set = new HashSet<>();
+        categoryService.findSubCategoryId(100001,set);
+        log.info("result is {}",set);
     }
 }
